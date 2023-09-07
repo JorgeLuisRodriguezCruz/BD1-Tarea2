2648cf5fdf2fcf31d0a7e8d1c647f93255698697
josafatbadilla <josafatbadilla@estudiantec.cr>
2023-09-07 02:09:33 -0600

    Creacion de Archivo para procesar datos de XML

---
+++ Tarea #2/XMLData.sql
@@ -0,0 +1,31 @@
+﻿CREATE PROCEDURE [dbo].[ChargeXML]
+	@xmlRoute varchar(200)
+
+AS
+	
+DECLARE @Data xml
+DECLARE @Comand NVARCHAR(500) = 'SELECT @Data =D FROM OPENROWSER (BULK ' + CHAR(39) + @xmlRoute + CHAR(39) + ', SINGLE_BLOB) AS Datos(D)'
+DECLARE @Parameters NVARCHAR(500)
+
+SET @Parameters = N'@Data xml OUTPUT' 
+
+
+EXECUTE sp_executesql @Comand, @Parameters, @Datos OUTPUT -- se ejecuta el comado que se hizo de manera dinamica
+
+DECLARE @hdoc int
+
+EXEC sp_xml_preparedocument @hdoc OUTPUT, @Data 
+
+INSERT INTO [dbo][Usuario]
+			([Usuario]
+			, [Clave])
+
+SELECT * FROM OPENXML (@hdoc, 'RUTA DENTRO DEL CATALOGO', 1)
+WITH (
+	USUARIO varchar
+	CLAVE varchar
+	)
+
+
+BEGIN
+END
